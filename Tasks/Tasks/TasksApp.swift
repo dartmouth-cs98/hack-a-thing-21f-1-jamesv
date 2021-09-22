3a62769fbf2d9a72bca4d5daf194c84d264d6f0a
jamesversch17 <jamesversch@gmail.com>
2021-09-22 15:13:46 -0400

    Showed failed tutorial

---
+++ Tasks/Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by James Verschleiser on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
